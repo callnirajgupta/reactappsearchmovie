01df45a3e76dc3327dd895316a6b0696da353d78
Niraj Gupta <callnirajgupta@gmail.com>
2021-02-19 07:24:47 +0530

    updated

--- src/SearchComponent.js
+++ src/SearchComponent.js
@@ -2,19 +2,20 @@ import React, {useState}  from "react"
 import MovieCard from "./MovieCard"
 
 export default  function SearchComponent(){
-    const [movies, setMovies]=useState([]);
-    const [query,setQuery]=useState("")
+  const [movies, setMovies]=useState([]);
+    const [query,setQuery]=useState('')
+  
 
   const searchMovies = async (e) => {
     e.preventDefault();
-    const url = `https://api.themoviedb.org/3/movie/550?api_key=ef9dc0d1c6e5daeae1f72cccba0838a6&language=en-US&query=${query}&page=1&include_adult=false`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
   try{
     const response= await fetch(url)
     const data= await response.json();
-    console.log(data)
-    setMovies(data.results)
+    console.log(data);
+    setMovies(data.results);
   }catch(err){
-      console.log(err)
+      console.error(err)
   }
    
 
@@ -35,9 +36,9 @@ export default  function SearchComponent(){
 
         </form>
         <div className="card-list">
-        
-          <MovieCard movies={movies}/>
-                
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                   <MovieCard movie={movie} key={movie.id} />
+                ))}
             </div>    
     </div>
     );
